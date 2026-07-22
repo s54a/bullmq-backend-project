f0d41dc94af813f505d705cf73734e3a5fb9fd53
Sooraj Gupta <soorajgupta00@gmail.com>
2026-07-22 21:09:15 +0530

    update

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
+import * as schema from "./schema.js";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
